02ff098862e6abc3ed8a8052e8fcac5570e0fa45
webwithyaroo <olawaleridollahi@gmail.com>
2024-11-24 10:57:23 -1100

    commiting hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -147,7 +147,8 @@ const Hero = () => {
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
+              Revolutionize
+              {/* redefi<b>n</b>e */}
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
